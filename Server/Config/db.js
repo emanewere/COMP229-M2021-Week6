a25e85e2ecae80bc81b5ae7c2f1d0cdeca1a23b7
Ofovwe Ewere <ewereeman@gmail.com>
2021-06-18 16:10:22 -0500

    first try with mongo atlas

--- Server/Config/db.js
+++ Server/Config/db.js
@@ -1,7 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.Secret = exports.HostName = exports.LocalURI = void 0;
+exports.Secret = exports.HostName = exports.RemoteURI = exports.LocalURI = void 0;
 exports.LocalURI = "mongodb://localhost/clothing_store";
-exports.HostName = "localhost";
+exports.RemoteURI = "mongodb+srv://Admin:bAKbkrRaPiu4yxFn@clustercomp229.xzazy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+exports.HostName = "RemoteHost";
 exports.Secret = "someSecret";
 //# sourceMappingURL=db.js.map
