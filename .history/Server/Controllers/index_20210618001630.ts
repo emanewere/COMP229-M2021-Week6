d728ad21e7d239a3d47f370854ef6bdee639ccea
Ofovwe Ewere <ewereeman@gmail.com>
2021-06-18 09:26:04 -0500

    added css to support authentication

--- .history/Server/Controllers/index_20210618001630.ts
+++ .history/Server/Controllers/index_20210618001630.ts
@@ -1,7 +1,5 @@
 import express, { Request, Response, NextFunction } from 'express';
 
-import passport from 'passport';
-import User from '../Models/user';
 // Display Functions
 
 export function DisplayHomePage(req: Request, res: Response, next: NextFunction): void
@@ -27,33 +25,4 @@ export function DisplayServicesPage(req: Request, res: Response, next: NextFunct
 export function DisplayContactPage(req: Request, res: Response, next: NextFunction): void
 {
     res.render('index', { title: 'Contact Us', page: 'contact'  });
-}
-
-export function DisplayLoginPage(req: Request, res: Response, next: NextFunction): void
-{
-    if(!req.user)
-    {
-        res.render('index', { title: 'Login', page: 'login', messages:req.flash('loginMessage')  });
-    }
-    
-}
-
-export function ProcessLoginPage(req: Request, res: Response, next: NextFunction): void
-{
-    
-}
-
-export function DisplayRegisterPage(req: Request, res: Response, next: NextFunction): void
-{
-
-}
-
-export function ProcessRegisterPage(req: Request, res: Response, next: NextFunction): void
-{
-    
-}
-
-export function ProcessLogoutPage(req: Request, res: Response, next: NextFunction): void
-{
-    
 }
